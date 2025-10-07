a21440bf189ad5ac132539d575b4f52b7677b81d
Alejandro Mederico <mederico.alejandro1988@gmail.com>
2025-10-07 00:30:38 -0300

    test pages frontend

--- frontend/src/pages/__testing__/Login.test.tsx
+++ frontend/src/pages/__testing__/Login.test.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
+import React from 'react';
 import { Router } from 'react-router-dom';
 
 import { authService } from '../../services/AuthService';
